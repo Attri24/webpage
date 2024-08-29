9ec0296fb368ea34fcc15326926cb22ca4a677bf
Attri Sharma <64518434+Attri24@users.noreply.github.com>
2024-08-29 19:55:05 +0530

    Update ProductDetails.js

--- pages/ProductDetails.js
+++ pages/ProductDetails.js
@@ -1,38 +1,4 @@
-// // src/pages/ProductDetails.js
-// import React, { useState } from 'react';
-// import ProductTable from '../components/ProductTable';
-// import CompareModal from '../components/CompareModal';
 
-// const ProductDetails = () => {
-//     const [compareList, setCompareList] = useState([]);
-//     const [isModalVisible, setIsModalVisible] = useState(false);
-
-//     const addToCompare = (product) => {
-//         if (compareList.length >= 4 || compareList.some(p => p.id === product.id)) {
-//             return;
-//         }
-//         setCompareList([...compareList, product]);
-//     };
-
-//     const removeProduct = (id) => {
-//         setCompareList(compareList.filter(product => product.id !== id));
-//     };
-
-//     return (
-//         <div className="ml-2 pt-2">
-//             <ProductTable addToCompare={addToCompare} />
-//             <CompareModal
-//                 visible={isModalVisible}
-//                 onClose={() => setIsModalVisible(false)}
-//                 products={compareList}
-//                 addToCompare={addToCompare}
-//             />
-//         </div>
-//     );
-// };
-
-// export default ProductDetails;
-// src/pages/ProductDetails.js
 import React from 'react';
 import ProductTable from '../components/ProductTable';
 
